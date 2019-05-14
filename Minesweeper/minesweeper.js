1286eca9d876fb9f9dc64b16068739771e604470
Simon Falkeid <simon@Simons-MacBook-Pro.local>
2019-05-14 14:36:21 +0200

    oj

--- Minesweeper/minesweeper.js
+++ Minesweeper/minesweeper.js
@@ -1,73 +1,140 @@
 // @ts-check
 
-function setup() {
-    let brett = [
-        [1, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
-        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 9, 1],
-        [0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1],
-        [0, 0, 1, 9, 1, 0, 1, 2, 9, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 1, 9, 2, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 9, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0, 0, 1, 2, 3, 2, 1, 0, 0, 0],
-        [0, 0, 1, 9, 1, 0, 0, 0, 1, 9, 9, 9, 1, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0, 0, 1, 2, 3, 2, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 9, 1, 0, 0],
-        [0, 0, 1, 9, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ]
-    let divBrett = document.getElementById("brett");
-    let ruter = Array.from(document.querySelectorAll("#brett > div"));
-    let i = 0;
-    for (let y = 0; y < 16; y++) {
-        for (let x = 0; x < 16; x++) {
-            let b = brett[y][x];
-            let r = ruter[i];
-            r.className = "rute";
-            r.dataset.idx = i;
-            if (b === 9) {
-                r.innerHTML = "O";
-            } else if (b !== 0) {
-                r.innerHTML = String(b);
-            }
-            i++;
-        }
-    }
-    divBrett.addEventListener("click", showMine);
+const W = 16;
+const H = 16;
 
-    function showMine(e) {
-        let t = e.target;
-        let idx = t.dataset.idx;
-        if (t.innerHTML === "O") {
-            document.getElementById("smily").innerHTML = ":(";
-            return;
+
+/**
+ * Lager et brett med gitt bredde x høyde
+ * @param {number} width Bredde på brett
+ * @param {number} height Høyde
+ * @returns {Array} array som representerer brettet
+ */
+function lagBrett(width, height) {
+    const TOTAL = width * height;
+    let b = Array(TOTAL).fill(0);
+    let antall = 35;
+    do {
+        let x = Math.trunc(Math.random() * TOTAL);
+        if (b[x] === 0) {
+            b[x] = 8;
+            antall--;
         }
-        let usett = Array.from(document.querySelectorAll("div.rute:not(.synlig)"));
-        if (usett.length === 11) {
-            document.getElementById("tid").innerHTML = "OK";
-            return;
-        }
+    } while (antall);
+    for (let i = 0; i < TOTAL; i++) {
+        let n = b[i];
+        if (n === 8) continue;
+        b[i] = tellOpp(b, i, width);
+    }
+    return b;
+}
+
+/**
+ * Henter verdi fra nabo-rute
+ * Gir tilbake 0 dersom utenfor brett
+ * @param {Array} brett Array over alle bomber
+ * @param {number} idx cellenr
+ * @param {number} w bredde på brett
+ * @param {number} n antall ruter totalt
+ * @param {number} dx delta x
+ * @param {number} dy delta y
+ * @returns {number} 1/0 1 dersom ruta har en 8
+ */
+const getAround = (brett, idx, w, n, dx, dy) => {
+    if (idx < w && dy === -1) return 0;
+    if (idx > n - w && dy === 1) return 0;
+    if (idx % w === 0 && dx === -1) return 0;
+    if ((idx + 1) % w === 0 && dx === 1) return 0;
+    return brett[idx + dx + w * dy] === 8 ? 1 : 0;
+}
+
+/**
+ * 
+ * @param {Array<number>} arr Array over bomber, 8=bombe
+ * @param {number} idx Ruten vi sjekker
+ * @param {number} w Bredde på brett
+ * @returns {number} Antall bomber rundt denne ruten
+ */
+function tellOpp(arr, idx, w) {
+    const adjacent = (dx, dy) => getAround(arr, idx, w, arr.length, dx, dy);
+    let n = 0;
+    n += adjacent(0, 1);
+    n += adjacent(0, -1);
+    n += adjacent(1, 0);
+    n += adjacent(-1, 0);
+    n += adjacent(1, 1);
+    n += adjacent(1, -1);
+    n += adjacent(-1, 1);
+    n += adjacent(-1, -1);
+    return n;
+}
+
+// viser bruk av funksjoner med parameter
+// bruker $ som i jquery og andre bib
+/**
+ * Gitt en id - returner ref til html-element
+ * @param {string} id Id på html-element
+ * @returns {HTMLElement}
+ */
+function $(id) {
+    return document.getElementById(id);
+}
+
+/**
+ * Viser brettet på skjermen
+ * Gir tilbake array over html-elementer for rutene
+ * @param {HTMLElement} div Div hvor brikker vises
+ * @param {Array} arr Plassering av miner
+ * @returns {Array<HTMLElement>}
+ */
+function visBrett(div, arr) {
+    div.innerHTML = "";
+    let r = [];
+    for (let i = 0; i < arr.length; i++) {
+        let b = arr[i];
+        let rute = document.createElement("div");
+        rute.className = "rute";
+        rute.dataset.idx = String(i);
+        rute.dataset.n = String(b);   // antall naboer som 
+        // er bomber, n === 8 markerer en bombe
+        div.appendChild(rute);
+        r.push(rute);
+    }
+    return r;
+}
+
+function setup() {
+    const brikker = lagBrett(W, H);
+    let divTid = $("tid");
+    let divAntall = $("antall");
+    let divBrett = $("brett");
+    let divSmily = $("smily");
+    let ruter = visBrett(divBrett, brikker);
+    divBrett.addEventListener("click", sjekkBombe);
+
+    function sjekkBombe(event) {
+        let t = event.target;
         if (t.classList.contains("rute")) {
-            reveal(idx);
-            function reveal(idx) {
-                let t = ruter[idx];
-                if (t.classList.contains("synlig")) {
-                    return;
-                }
-                t.classList.add("synlig");
-                if (t.innerHTML !== "") {
-                    return;
-                }
-                if (idx < 16 || idx > 238 || idx % 16 === 0 || (idx + 1) % 16 === 0) {
-                    // sadhkh
-                } else {
-                    reveal(idx - 1);
-                    reveal(idx + 1);
-                    reveal(idx + 16);
-                    reveal(idx - 16);
+            let idx = Number(t.dataset.idx);
+            if (t.classList.contains("rute")) {
+                reveal(idx);
+                function reveal(idx) {
+                    let t = ruter[idx];
+                    if (t.classList.contains("visible")) {
+                        return;
+                    }
+                    t.classList.add("visible");
+                    if (t.dataset.n !== "0") {
+                        return;
+                    }
+                    if (idx < W || idx > 238 || idx % W === 0 || (idx + 1) % W === 0) {
+                        // sadhkh
+                    } else {
+                        setTimeout(() => reveal(idx - 1), 100);
+                        setTimeout(() => reveal(idx + 1), 100);
+                        setTimeout(() => reveal(idx - 16), 100);
+                        setTimeout(() => reveal(idx + 16), 300);
+                    }
                 }
             }
         }
